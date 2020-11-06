6c5ab8e3e71935afa5808a55a9deac1a06588bb9
Vuli Russell <vulirussell@gmail.com>
2020-11-06 13:31:35 +0000

    removing unnecessary cors

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-// app.use((request,response,next)=>{
-//     response.header("Access-Control-Allow-Origin", "*");
-//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
 
 app.get("/trees", async (request,response) => {
     const trees = await getCollectionDocuments("data")
