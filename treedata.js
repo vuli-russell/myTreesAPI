5049fa058ed6def8bc166850f63e7ae8daba4cd2
Vuli Russell <vulirussell@gmail.com>
2020-11-06 10:43:41 +0000

    commiting api

---
+++ treedata.js
@@ -0,0 +1,37 @@
+export const treeData = [
+    {
+        id: 1,
+        name: "Alder",
+        description: "Swamp-dweller, water-lover. The wood of this tough tree doesn’t rot when waterlogged, instead turning stronger and harder."
+    },
+    {
+        id: 2,
+        name: "Alder buckthorn",
+        description: "A reputation for causing a bang. Alder buckthorn is used to make gunpowder, pigments and dyes. It’s a beloved plant of the brimstone butterfly."
+    },
+    {
+        id: 3,
+        name: "Apple",
+        description: "Tart, tangy and crisp. The fruit of the apple tree is a firm favourite in the UK. And although they’re not native, we’ve been breeding them for centuries as eaters, cookers and to make cider."
+    },
+    {
+        id: 4,
+        name: "Apple, crab",
+        description: "A symbol of fertility and a forager's delight. Crab apple trees are associated with love and marriage and its small, hard fruits make an exquisite, jewel-coloured jelly."
+    },
+    {
+        id: 5,
+        name: "Ash",
+        description: "One of our most beloved trees. Ash is one of the most common trees in the UK, but as ash dieback sweeps through, is it set to be erased from our countryside?"
+    },
+    {
+        id: 6,
+        name: "Aspen",
+        description: "Trembling, fluttering and shimmering in the slightest breeze. The rippling leaves of this beautiful tree give it its name: quaking aspen."
+    },
+    {
+        id: 7,
+        name: "Beech, common",
+        description: "Monumental, majestic, home to rare wildlife. Beech is an enchanting species and known as the queen of British trees. To wander beneath the leafy canopy, its cathedral-like branches spreading upwards, is an awe-inspiring experience."
+    }
+]
